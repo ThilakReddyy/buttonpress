89478969ff455d08ee87a5f2f3a70a7816f3012e
thilakreddy <thilakreddy19102000@gmail.com>
2024-06-15 17:20:52 +0530

    totalButtonpresses

--- app/api/updateButtonClicks/route.ts
+++ app/api/updateButtonClicks/route.ts
@@ -1,16 +1,66 @@
 // pages/api/updateButtonClicks.js
 
-var totalButtonPresses = 0;
-export async function POST(request: Request) {
-  totalButtonPresses++;
-  return Response.json(
-    { totalButtonPresses: totalButtonPresses },
-    { status: 200 },
-  );
+import { promises as fs } from "fs";
+import path from "path";
+
+const filePath = path.resolve("./data", "buttonPresses.json");
+
+// Global variable to store the count
+let totalButtonPresses = 0;
+
+// Load the initial value from the file when the server starts
+async function loadInitialValue() {
+  try {
+    const data = await fs.readFile(filePath, "utf-8");
+    const { totalButtonPresses: initialCount } = JSON.parse(data);
+    totalButtonPresses = initialCount;
+  } catch (error) {
+    console.error("Error loading initial value:", error);
+  }
 }
+
+// Save the current value to the file
+async function saveCurrentValue() {
+  try {
+    await fs.writeFile(
+      filePath,
+      JSON.stringify({ totalButtonPresses }),
+      "utf-8",
+    );
+  } catch (error) {
+    console.error("Error saving current value:", error);
+  }
+}
+
+// Load the initial value when the server starts
+loadInitialValue();
+
+export async function POST(request: Request) {
+  try {
+    totalButtonPresses++;
+    await saveCurrentValue();
+    return Response.json(
+      { totalButtonPresses: totalButtonPresses },
+      { status: 200 },
+    );
+  } catch (error) {
+    return Response.json(
+      { totalButtonPresses: totalButtonPresses },
+      { status: 500 },
+    );
+  }
+}
+
 export async function GET(request: Request) {
-  return Response.json(
-    { totalButtonPresses: totalButtonPresses },
-    { status: 200 },
-  );
+  try {
+    return Response.json(
+      { totalButtonPresses: totalButtonPresses },
+      { status: 200 },
+    );
+  } catch (error) {
+    return Response.json(
+      { totalButtonPresses: totalButtonPresses },
+      { status: 500 },
+    );
+  }
 }
